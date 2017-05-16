16152a98ad0130b59dc136d1e3351c908ad42121
Vquillien <vquillien56@gmail.com>
2017-05-16 15:01:29 +0200

    First try of achitecture
    Creation an achitecture and pakage for obisdata.

---
+++ filter/fes-2.0.2/src/main/java/org/orbisgis/orbisdata/filter/fes_2_0_2/ParserWriterFesXmlToSql.java
@@ -0,0 +1,8 @@
+package org.orbisgis.orbisdata.filter.fes_2_0_2;
+
+/**
+ * Created by quillien on 16/05/2017.
+ */
+public class ParserWriterFesXmlToSql {
+
+}
---
+++ filter/fes-2.0.2/src/test/java/org/orbisgis/orbisdata/filter/fes_2_0_2/TestParserWriterFes2_0_2.java
@@ -0,0 +1,7 @@
+package org.orbisgis.orbisdata.filter.fes_2_0_2;
+
+/**
+ * Created by quillien on 16/05/2017.
+ */
+public class TestParserWriterFes2_0_2 {
+}

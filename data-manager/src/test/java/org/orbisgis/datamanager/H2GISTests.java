fdcc37484c6578f21569016f1091df2100d74b92
ebocher <erwan.bocher@univ-ubs.fr>
2018-12-18 14:33:52 +0100

    Prepare tests

--- data-manager/src/test/java/org/orbisgis/datamanager/H2GISTests.java
+++ data-manager/src/test/java/org/orbisgis/datamanager/H2GISTests.java
@@ -2,6 +2,7 @@ package org.orbisgis.datamanager;
 
 import groovy.lang.Closure;
 import groovy.sql.GroovyResultSet;
+import org.h2gis.utilities.SpatialResultSet;
 import org.junit.jupiter.api.Test;
 import org.locationtech.jts.geom.Geometry;
 import org.osgi.service.jdbc.DataSourceFactory;
@@ -10,6 +11,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -45,10 +48,33 @@ public class H2GISTests {
                 return argument;
             }
         });
-
         assertTrue(values.size()==2);
         assertTrue(values.contains("[THE_GEOM:POINT (10 10)]"));
         assertTrue(values.contains("[THE_GEOM:POINT (1 1)]"));
+    }
+
+    @Test
+    public void querySpatialTable() throws SQLException {
+        Map<String, String> map = new HashMap<String, String>();
+        map.put(DataSourceFactory.JDBC_DATABASE_NAME, "./target/loadH2GIS");
+        H2GIS h2GIS = H2GIS.open(map);
+        h2GIS.execute("DROP TABLE IF EXISTS h2gis; CREATE TABLE h2gis (id int, the_geom point);insert into h2gis values (1, 'POINT(10 10)'::GEOMETRY), (2, 'POINT(1 1)'::GEOMETRY);");
+
+        ArrayList<String> values = new ArrayList<>();
+        h2GIS.getSpatialTable("h2gis").eachRow(new Closure(null){
+            @Override
+            public Object call(Object argument) {
+                try {
+                    values.add(((SpatialResultSet)argument).getGeometry().toString());
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                return argument;
+            }
+        });
+        assertEquals(2,values.size());
+        assertTrue(values.contains("POINT (10 10)]"));
+        assertTrue(values.contains("POINT (1 1)]"));
 
     }
 }
